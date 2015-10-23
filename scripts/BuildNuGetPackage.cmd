b543b5f5e41dc8bbbc4dc744d8d206413e03e180
Rob Relyea <rob@relyeas.net>
2015-10-23 12:56:07 -0700

    Fix NuGet package build
    All set to build x64 debug packages. Change BuildNuGetPackage.cmd when
    you need to build x86 or retail.

--- scripts/BuildNuGetPackage.cmd
+++ scripts/BuildNuGetPackage.cmd
@@ -1,1 +1,1 @@
-nuget.exe pack Microsoft.Wpf.Interop.DirectX.nuspec -Version 0.9.0-beta-99999 -Properties ArchitecturePublicName=x64
+nuget.exe pack Microsoft.Wpf.Interop.DirectX.nuspec -Version 0.9.0-beta-99999 -Properties ArchitecturePublicName=x64;NuGetBinaries=..\src\x64\debug
